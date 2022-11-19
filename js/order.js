cd80d01790583055203ac0d38ad98798c4fbf898
Leszek Cimała <ernierasta@zori.cz>
2022-11-19 14:33:55 +0100

    fix countdown display

--- js/order.js
+++ js/order.js
@@ -14,19 +14,16 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  // Output the result in an element with id="countdown"
+  document.getElementById("countdown").innerHTML = "<b>" + minutes + ":" + seconds + "</b>";
     
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Sesja wygasła. :-(";
+    document.getElementById("countdown").innerHTML = "Sesja wygasła. :-(";
   }
 }, 1000);
 
